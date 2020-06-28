713a4014174d17f6832d182aff51ddc72bb004b7
eronde <ghe@nemo-site.eu>
2020-06-28 15:17:46 +0200

    Add nvim files

--- makeDotSymlinks.sh
+++ makeDotSymlinks.sh
@@ -2,11 +2,17 @@
 DOTDIR=~/Dotfiles
 DOTDIR_OLD=~/Dotfiles_OLD
 DOTFILES="bashrc vim vim/vimrc tmux.conf"
+
+CONFIGDIR=~/.conf
+CONFIGFILES="nvim"
+CONFIGFILES_OLD=~/config_old
 NOW=$(date +%d%m%y%H%M%S)
 
 mkdir -vp $DOTDIR
 mkdir -vp $DOTDIR_OLD
+mkdir -vp $CONFIGFILES_OLD
 
+#Dotfiles
 for FILE in $DOTFILES ; do
   if [ -e ~/.$FILE ]; then
     echo "Moving old ~./$FILE to $DOTDIR_OLD/"
@@ -14,5 +20,15 @@ for FILE in $DOTFILES ; do
   fi
   echo "Symlinking $DOTDIR/$FILE"
   ln -vs $DOTDIR/$FILE ~/.$FILE
+done
+
+#Dotfiles
+for FILE in $CONFIGFILES ; do
+  if [ -e ~/$CONFIGDIR/$FILE ]; then
+    echo "Moving old ~./$CONFIGDIR/$FILE to $CONFIGFILES_OLD/"
+    mv -v  ~/$CONFIGFILES/$FILE $CONFIGFILES_OLD/$FILE\_$NOW
+  fi
+  echo "Symlinking $DOTDIR/$FILE"
+  ln -vs $DOTDIR/$FILE ~/$CONFIGDIR/$FILE
 done
 
