208c024c26f5b191c5947e3a1da0e66a833d4141
eronde <ghe@nemo-site.eu>
2021-04-24 16:48:58 +0200

    zsh: update zsh dir && add plugin: zsh-vim-mode, zsh-system-clipboard.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -10,8 +10,9 @@
 
 
 source ~/Dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
-# source ~/Dotfiles/zsh/plugins/zsh-kjk 2>/dev/null
 
 source ~/Dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 2>/dev/null
 source ~/Dotfiles/zsh/plugins/zsh-edit/zsh-edit.plugin.zsh 2>/dev/null
 source ~/Dotfiles/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh 2>/dev/null
+source ~/Dotfiles/zsh/plugins/zsh-vim-mode/zsh-vim-mode.plugin.zsh 2>/dev/null
+source ~/Dotfiles/zsh/plugins/zsh-system-clipboard/zsh-system-clipboard.zsh 2>/dev/null
